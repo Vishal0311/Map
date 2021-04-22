6b5ff8fc872491a17da7d20a80ebd7a4f45008f3
Vishal0311 <kovishal27@gmail.com>
2021-04-23 01:18:31 +0530

    new change

--- app/src/main/java/com/vishal/newmap/MainActivity.java
+++ app/src/main/java/com/vishal/newmap/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.os.Looper;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -53,6 +54,8 @@ public class MainActivity extends AppCompatActivity {
                 for (Location location : locationResult.getLocations()) {
                     // Update UI with location data
                     // ...
+                    Log.d("Latitude", "onLocationResult"+ location.getLatitude());
+                    Log.d("Longitude", "onLocationResult"+ location.getLongitude());
                 }
             }
         };
