9858237423b999afb10d70ee519e833257e510f7
Vishal0311 <kovishal27@gmail.com>
2021-04-25 19:37:30 +0530

    added night theme map - sunday

--- app/src/main/java/com/vishal/newmap/MainActivity.java
+++ app/src/main/java/com/vishal/newmap/MainActivity.java
@@ -2,9 +2,11 @@ package com.vishal.newmap;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.location.Location;
 import android.os.Bundle;
 import android.os.Looper;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -17,74 +19,68 @@ import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity  implements OnMapReadyCallback {
-
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     Button bt_loc;
     TextView textView1, textView2, textView3, textView4, textView5;
     FusedLocationProviderClient fusedLocationProviderClient;
     private LocationCallback locationCallback;
-
+    private Location mlocation = null;
     private GoogleMap mMap;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(MainActivity.this);
-
         bt_loc = findViewById(R.id.loc_button);
         textView1 = findViewById(R.id.textView1);
         textView2 = findViewById(R.id.textView2);
-
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         bt_loc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
-                    getlocation();
-                else {
-                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+                if (ActivityCompat
+                        .checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED && ActivityCompat
+                        .checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED) {
+                    // We should set up map here
+                } else {
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
                 }
             }
         });
-        locationCallback = new LocationCallback() {
-            @Override
-            public void onLocationResult(LocationResult locationResult) {
-                if (locationResult == null) {
-                    return;
-                }
-                for (Location location : locationResult.getLocations()) {
-                    // Update UI with location data
-                    // ...
-                    textView1.setText(String.valueOf(location.getLatitude()));
-                    textView2.setText(String.valueOf(location.getLongitude()));
-                }
-            }
-        };
-
-    }
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
-
-    private void getlocation() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+   /* locationCallback = new LocationCallback() {
+      @Override
+      public void onLocationResult(LocationResult locationResult) {
+        if (locationResult == null) {
+          return;
+        }
+        for (Location location : locationResult.getLocations()) {
+          // Update UI with location data
+          // ...
+          textView1.setText(String.valueOf(location.getLatitude()));
+          textView2.setText(String.valueOf(location.getLongitude()));
+        }
+      }
+    };
+*/
+    }  private void getlocation(LocationResultCallback callback) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
@@ -93,18 +89,57 @@ public class MainActivity extends AppCompatActivity  implements OnMapReadyCallba
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
             return;
+        }    LocationRequest locationRequest = LocationRequest.create()
+                .setInterval(5000)
+                //.setSmallestDisplacement(10F) // In meters
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);    fusedLocationProviderClient
+                .requestLocationUpdates(locationRequest, new LocationCallback() {
+                            @Override
+                            public void onLocationResult(LocationResult locationResult) {
+                                if (locationResult == null) {
+                                    return;
+                                }
+                                callback.opUpdate(locationResult.getLastLocation());
+                                for (Location location : locationResult.getLocations()) {
+                                    // Update UI with location data
+                                    textView1.setText(String.valueOf(location.getLatitude()));
+                                    textView2.setText(String.valueOf(location.getLongitude()));
+                                }
+                            }
+                        },
+                        Looper.getMainLooper());
+    }  @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        try {
+            // Customise the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            boolean success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.nightmode));
+
+            if (!success) {
+                Log.e("msg: ", "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e("msg: ", "Can't find style. Error: ", e);
         }
 
-        LocationRequest locationRequest = LocationRequest.create()
-                .setInterval(1000)
-                .setSmallestDisplacement(10F) // In meters
-                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
-        fusedLocationProviderClient.requestLocationUpdates(locationRequest,locationCallback, Looper.getMainLooper());
-
-
+        mMap = googleMap;    if (mlocation != null) {
+            // Not implemented
+        }
+        getlocation((location -> {
+            Log.d("setOnClickListener", "onClick: location from callback " + location);
+            // Add a marker in Sydney and move the camera
+            LatLng sydney = new LatLng(location.getLatitude(), location.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+            CameraUpdate center = CameraUpdateFactory.newLatLng(sydney);
+            CameraUpdate zoom = CameraUpdateFactory.zoomTo(15);
+            mMap.moveCamera(center);
+            mMap.animateCamera(zoom);
+            //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,18f));
+        }));
+    }  interface LocationResultCallback {    public void opUpdate(Location location);
     }
-
-
-
 }
